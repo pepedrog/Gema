2a49af10109c6f5c9f7f2d7da6a357a287c3b049
pepedrog <gigeck.pedro@hotmail.com>
2020-05-17 15:20:04 -0300

    lee e preparata feito mas não testado

--- geocomp/triangulação/lee_preparata.py
+++ geocomp/triangulação/lee_preparata.py
@@ -5,10 +5,113 @@ Pedro Gigeck Freire - nUSP 10737136
 """
 
 from geocomp.common.segment import Segment
-from geocomp.common.prim import right
+from geocomp.common.abbb import Abbb
+from geocomp.common.prim import left, right_on, left_on
 from geocomp.common import control
 from .monotono import Monotono
     
+# todo: fazer a dcel
+# Debugar a inserção na abbb
+
+class Trapezio:
+    # Recebe o vértice de superte v e as duas arestas da esquerda e direita
+    def __init__ (self, sup, e = None, d = None):
+        self.sup = sup
+        if e != None and d != None:
+            if e.init.y < e.to.y:
+                e.init, e.to = e.to, e.init
+            if d.init.y < d.to.y:
+                d.init, d.to = d.to, d.init
+            self.a_esq = e
+            self.a_dir = d
+    
+    def __eq__ (self, other):
+        return (other != None and other.sup.y <= self.sup.y and
+                right_on (self.a_esq.init, self.a_esq.to, other.sup) and
+                left_on (self.a_dir.init, self.a_dir.to, other.sup))
+    
+    def __gt__ (self, other):
+        return left (self.a_esq.init, self.a_dir.to, other.sup)
+        
+def ponta_pra_baixo (p):
+    return p.next.y > p.y and p.prev.y > p.y
+
+def trata_caso_meio (p, viz_baixo, L, diags):
+    # Remove da linha o trapésio que tem o p
+    t = Trapezio (p)
+    removido = (L.busca (t)).elemento
+    L.deleta (t)
+    
+    # Insere um novo trapésio com o p
+    # Se o removido estava a direita
+    if (p == removido.a_esq.to):
+        t.a_dir = Segment (p, viz_baixo)
+        t.a_esq = removido.a_esq        
+    # Se estava a esquerda
+    else:
+        t.a_esq = Segment (p, viz_baixo)
+        t.a_dir = removido.a_dir
+    L.insere (t)
+    
+    if ponta_pra_baixo (removido.sup):
+        d = Segment (removido.sup, p)
+        d.plot()
+        control.sleep()
+        diags.append (d)
+    
+def trata_ponta_pra_cima (p, L, diags):
+    viz_esq = p.next
+    viz_dir = p.prev
+    if viz_esq.x > viz_dir.x:
+        viz_esq, viz_dir = viz_dir, viz_esq
+    
+    t = Trapezio (p)
+    removido = (L.busca (t)).elemento
+    L.deleta (t)
+    
+    if removido == None:
+        t.a_esq = Segment (p, viz_esq)
+        t.a_dir = Segment (p, viz_dir)
+        L.insere (t)
+    
+    else:
+        t1 = Trapezio (p, removido.a_esq, Segment (p. viz_dir))
+        t2 = Trapezio (p, Segment (p, viz_esq), removido.a_dir)
+        L.insere (t1)
+        L.insere (t2)
+        
+        d = Segment (p, removido.sup)
+        d.plot()
+        control.sleep()
+        diags.append (d)
+    
+def trata_ponta_pra_baixo (p, L, diags):
+    t = Trapezio (p)
+    removido1 = (L.busca (t)).elemento
+    L.deleta (t)
+    
+    if ponta_pra_baixo (removido1.sup):
+        d = Segment (removido1.sup, p)
+        d.plot()
+        control.sleep()
+        diags.append (d)
+    
+    # Se tem outro polígono
+    removido2 = (L.busca (t)).elemento
+    if removido2 != None:
+        L.deleta (t)
+        
+        if ponta_pra_baixo (removido2.sup):
+            d = Segment (removido2.sup, p)
+            d.plot()
+            control.sleep()
+            diags.append (d)
+        
+        if removido2.a_esq.to == p:
+            L.insere (p, removido1.a_esq, removido2.a_dir)
+        else:
+            L.insere (p, removido2.a_esq, removido1.a_dir)
+
 def monotonos (P):
     """ Função que recebe um polígono P e particiona P em vários polígonos monótonos
         Através da inserção de diagonais
@@ -17,6 +120,9 @@ def monotonos (P):
     # Ordena os vértices pela Y-coordenada
     v = P.vertices()
     v = sorted(v, key = lambda x:(-x.y))
+    
+    diags = [] # lista de diagonais que acrescentamos
+    L = Abbb()
     
     # os vértices são os pontos eventos da linha de varredura
     for p in v:
@@ -30,13 +136,11 @@ def monotonos (P):
             viz_cima, viz_baixo = viz_baixo, viz_cima
         
         if viz_cima.y > p.y and p.y > viz_baixo.y:
-            trata_caso_meio()
-        elif viz_baixo.y > p.y:
-            trata_ponta_pra_baixo()
+            trata_caso_meio (p, viz_baixo, L, diags)
+        elif viz_cima.y < p.y:
+            trata_ponta_pra_cima (p, L, diags)
         else:
-            trata_ponta_pra_cima()
-        
-        
+            trata_ponta_pra_baixo (viz_cima, p, viz_baixo, L, diags) 
     
     control.sleep()
         
