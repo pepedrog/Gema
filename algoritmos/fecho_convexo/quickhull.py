cf116e125036a6a02de10395e6d9fd7e2632ca15
pepedrog <gigeck.pedro@hotmail.com>
2020-07-23 17:49:30 -0300

    novos inputs segmentos

--- algoritmos/fecho_convexo/quickhull.py
+++ algoritmos/fecho_convexo/quickhull.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 """
 Algoritmo QuickHull para determinação do fecho convexo
-Pedro Gigeck Freire 10737136
+Consumo de tempo = O(nh) ou O(n*lgn) (o que for melhor)
+onde n é o número de pontos e h o número de pontos no fecho
 """
 
 from estruturas.prim import left, right, area2
@@ -10,6 +11,11 @@ from desenhos import sleep, plot_delete
 eps = 1e-6
 
 def particione (P, l, r):
+    """ Particiona o conjunto de pontos P em 3 partes:
+        A esquerda do triangulo l-r-x
+        A direita do triangulo l-r-x
+        Dentro do triangulo l-r-x, 
+        onde x é o ponto em P que maximiza a área do triangulo """
     # P[l + 1] recebe ponto extremo
     for i in range(l + 1, r):
         P[i].lineto(P[l], "gray")
@@ -21,14 +27,12 @@ def particione (P, l, r):
            (abs(area2(P[i], P[l], P[r]) - area2(P[l + 1], P[l], P[r])) < eps and
             left(P[l], P[l + 1], P[i]))):
             P[i], P[l + 1] = P[l + 1], P[i]
-
     # Desenha o triangulo
     if hasattr(P[l + 1], 'hi'): P[l + 1].unhilight()
     P[l + 1].hilight("firebrick")
     linha_esq = P[l].lineto(P[l + 1], "green")
     linha_dir = P[r].lineto(P[l + 1], "red")
     sleep()
-    
     p = q = r
     for k in range(r - 1, l + 1, -1):
         if hasattr(P[k], 'hi'): P[k].unhilight()
@@ -49,7 +53,6 @@ def particione (P, l, r):
             if p != q: P[p], P[k] = P[k], P[p]
             P[q].hilight("red")
             sleep()
-    
     # Ajustes finais no vetor
     p -= 1
     q -= 1
@@ -62,6 +65,7 @@ def particione (P, l, r):
     return p, q, linha_esq, linha_dir
 
 def quickhull_rec (P, l, r):
+    """ Função principal do algoritmo """
     if r - l == 1:
         P[l].hilight()
         P[r].hilight()
